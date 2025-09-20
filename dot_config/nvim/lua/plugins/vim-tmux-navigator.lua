61db198d56274b3f4f99ea3bbbda5efd7d56aed8
Trent Haines <thaines@roblox.com>
2025-09-20 11:42:16 -0700

    tmux sessionizer thank god

--- dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,4 +8,18 @@ return {
     { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right" },
     { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Navigate previous" },
   },
+  config = function()
+    -- Add window swapping keymaps that mirror tmux (prefix + Shift+hjkl)
+    -- Using <leader> as the "prefix" equivalent in vim
+    vim.keymap.set("n", "<leader>H", "<C-w>H", { desc = "Move window left" })
+    vim.keymap.set("n", "<leader>J", "<C-w>J", { desc = "Move window down" })
+    vim.keymap.set("n", "<leader>K", "<C-w>K", { desc = "Move window up" })
+    vim.keymap.set("n", "<leader>L", "<C-w>L", { desc = "Move window right" })
+
+    -- Also add lowercase for navigation (matching tmux prefix+hjkl fallback)
+    vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Navigate window left" })
+    vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Navigate window down" })
+    vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Navigate window up" })
+    vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Navigate window right" })
+  end,
 }
