504cc594ef282b9522fc813a70da53765458da70
Trent Haines <thaines@roblox.com>
2025-10-01 16:23:10 +0200

    a

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,3 +4,16 @@
 
 -- Force tokyonight colorscheme
 vim.g.lazyvim_colorscheme = "tokyonight"
+
+-- Auto-reload files when changed externally
+vim.o.autoread = true
+
+-- Check for file changes on these events
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
+-- Disable snacks scroll animations
+vim.g.snacks_animate = false
+
