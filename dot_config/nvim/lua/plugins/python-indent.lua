40cefeec874c4c89703a4aeed033e208da103d78
Trent Haines <thaines@roblox.com>
2025-10-16 21:20:43 -0700

    a

---
+++ dot_config/nvim/lua/plugins/python-indent.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "Vimjas/vim-python-pep8-indent",
+    ft = "python",
+  },
+}
---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- show hidden files
+        ignored = true, -- show .gitignore files
+      },
+    },
+  },
+}
