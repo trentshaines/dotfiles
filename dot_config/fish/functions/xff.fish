4ffe59b33499c6f22c7dbbd7004eca92c9c84264
Trent Haines <trent.haines@decagon.ai>
2026-01-26 18:53:03 -0800

    eff/xff: add headers to show edit vs execute

--- dot_config/fish/functions/xff.fish
+++ dot_config/fish/functions/xff.fish
@@ -1,6 +1,6 @@
 # Execute fish function with fzf
 function xff --description "Execute fish function with fzf"
-    set -l func_name (ls ~/.config/fish/functions/*.fish | xargs -n1 basename | sed 's/\.fish$//' | fzf --preview 'functions {}' --preview-window=right:60%)
+    set -l func_name (ls ~/.config/fish/functions/*.fish | xargs -n1 basename | sed 's/\.fish$//' | fzf --header '🚀 EXECUTE function' --preview 'functions {}' --preview-window=right:60%)
     if test -n "$func_name"
         echo "Running: $func_name"
         eval $func_name
