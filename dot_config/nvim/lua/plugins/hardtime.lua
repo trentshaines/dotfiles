20a21ea3b5da15fcb36b49526b37a631934b8b05
Trent Haines <trentshaines@gmail.com>
2026-07-05 18:28:03 -0700

    hardtime.nvim: use hint mode instead of blocking keys

--- dot_config/nvim/lua/plugins/hardtime.lua
+++ dot_config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,8 @@ return {
   "m4xshen/hardtime.nvim",
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
+  opts = {
+    -- Warn/hint instead of blocking keys ("block" is the default)
+    restriction_mode = "hint",
+  },
 }
